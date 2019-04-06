0b55c7919571b4f028f35652c68c914dc2c00924
michaelagaz <miska@gazo.home.sk>
2019-04-06 13:23:35 +0200

    samplecontroller

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('list', 'SampleController@index');
+Route::get('', 'SampleController@index');
